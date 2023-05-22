044093f777b07737d4f6aaeda2d5f363bf6cef25
RYU HYE RANG <124871583+helena7645@users.noreply.github.com>
2023-05-22 16:27:37 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let signupBtn = document.getElementById('signupBtn')
+let signinBtn = document.getElementById('signinBtn')
+let nameField = document.getElementById('nameField')
+let title = document.getElementById('title')
+
+signinBtn.addEventListener('click', function () {
+  nameField.style.maxHeight = '0'
+  title.innerHTML = 'Sign In'
+  signupBtn.classList.add('disable')
+  signinBtn.classList.remove('disable')
+})
+
+signupBtn.addEventListener('click', function () {
+  nameField.style.maxHeight = '65px'
+  title.innerHTML = 'Sign Up'
+  signupBtn.classList.remove('disable')
+  signinBtn.classList.add('disable')
+}) 
